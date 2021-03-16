3647dc4f51fbcd47d73600f00fe56c65ef2d2be0
Gerardo Lanza <60949087+glanza007@users.noreply.github.com>
2021-03-16 15:53:03 -0600

    categories

---
+++ Shops/Controllers/CategoriesController.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shops.Data;
+using Shops.Data.Entities;
+using Shops.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Shops.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public CategoriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+        // GET: Categories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CategoryViewModel view)
+        {
+            if (ModelState.IsValid)
+            {
+                var path = string.Empty;
+
+                if (view.ImageFile != null && view.ImageFile.Length > 0)
+                {
+                    path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\Categories", view.ImageFile.FileName);
+
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await view.ImageFile.CopyToAsync(stream);
+                    }
+
+                    path = $"~/images/Categories/{view.ImageFile.FileName}";
+                }
+
+                var category = this.ToCategory(view, path);
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+                // TODO: Pending to change to: this.User.Identity.Name
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(view);
+        }
+
+        private Category ToCategory(CategoryViewModel view, string path)
+        {
+            return new Category
+            {
+                Id = view.Id,
+                ImageUrl = path,
+                Name = view.Name
+            };
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            var view = this.ToCategoryViewModel(category);
+            return View(view);
+        }
+        private CategoryViewModel ToCategoryViewModel(Category category)
+        {
+            return new CategoryViewModel
+            {
+                Id = category.Id,
+                ImageUrl = category.ImageUrl,
+                Name = category.Name,
+            };
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(CategoryViewModel view)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var path = view.ImageUrl;
+
+                if (view.ImageFile != null && view.ImageFile.Length > 0)
+                {
+                    path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\Categories", view.ImageFile.FileName);
+
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await view.ImageFile.CopyToAsync(stream);
+                    }
+
+                    path = $"~/images/Categories/{view.ImageFile.FileName}";
+                }
+
+                // TODO: Pending to change to: this.User.Identity.Name
+
+                var category = this.ToCategory(view, path);
+                _context.Update(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(view);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Shops/Models/CategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Shops.Data.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shops.Models
+{
+    public class CategoryViewModel : Category
+    {
+
+        [Display(Name = "Imagen")]
+        public IFormFile ImageFile { get; set; }
+
+    }
+}
+
